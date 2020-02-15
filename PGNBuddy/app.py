8646cbe2972429072841793ffec36f1daacafd89
trevor-ofarrell <744@holbertonschool.com>
2020-02-14 18:40:53 -0800

    setting up firebase auth

--- PGNBuddy/app.py
+++ PGNBuddy/app.py
@@ -1,13 +1,37 @@
 #!/usr/bin/python3
-from flask import Flask, jsonify, render_template
+from flask import *
 from PGNBuddy.views import app_views
+import pyrebase
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
-@app.route('/')
-def home():
+config = {
+    #Fire base config info here
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+#email = input('enter email')
+#password = input('enter password')
+#user = auth.create_user_with_email_and_password(email, password)
+#user = auth.sign_in_with_email_and_password(email, password)
+#auth.send_email_verification(user['idToken'])
+#print(auth.get_account_info(user['idToken']))
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    succesful = "Login succesful"
+    unsuccesful = 'incorrect email or password'
+    if request.method == 'POST':
+        email = request.form['name']
+        password = request.form['pass']
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            return render_template('index.html', s=succesful)
+        except:
+            return render_template('index.html', us=unsuccesful)
     return render_template('index.html')
 
 @app.errorhandler(404)

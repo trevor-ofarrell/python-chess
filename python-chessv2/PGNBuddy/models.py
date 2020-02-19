ad12e1096da60b7be006c960e07082d58eb24000
trevor-ofarrell <744@holbertonschool.com>
2020-02-18 23:21:54 -0800

    playing with the front end a bit and preparing to build the user dashboard

--- python-chessv2/PGNBuddy/models.py
+++ python-chessv2/PGNBuddy/models.py
@@ -7,3 +7,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class pgn(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    game = db.Column(db.String(9999))
+    name = db.Column(db.String(1000))

6b5b4fd836dba1bfb4c2dacf10ef463f0e0ffccd
trevor-ofarrell <744@holbertonschool.com>
2020-03-02 11:17:52 -0800

    removing unneeded files, updating readme

--- PGNBuddy/auth.py
+++ PGNBuddy/auth.py
@@ -29,7 +29,7 @@ def login_post():
         return redirect(url_for('auth.login'))
 
     login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('main.dashboard'))
 
 @auth.route('/signup')
 def signup():
